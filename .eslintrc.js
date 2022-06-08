adf4ca951d10a3b4d902df95cdf50cd962df9892
ZMYGGG <1511049591@qq.com>
2022-06-08 09:48:52 +0800

    add eslint setting of reacthooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,18 @@
 module.exports={
     "parser":'babel-eslint',
     "extends":"airbnb-base",
+    plugins:[
+        "react-hooks"
+    ],
     "env": {
         "browser": true,
         "node": true
     },
     rules:{
-        'no-tabs':'off'
+        'no-tabs':'off',
+        // 检查 Hooks 的使用规则
+        "react-hooks/rules-of-hooks": "error", 
+        // 检查依赖项的声明
+        "react-hooks/exhaustive-deps": "warn"
     }
 }
